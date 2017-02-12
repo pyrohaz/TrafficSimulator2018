9834d52f1f53968779d5fb3c7d4fb3032d8d56e8
RichardInnocent <richardinnocent@sky.com>
2017-02-12 23:54:47 +0000

    Added some stuff

--- TrafficSimulator2018/Person.cs
+++ TrafficSimulator2018/Person.cs
@@ -15,8 +15,50 @@ namespace TrafficSimulator2018
 	/// </summary>
 	public class Person
 	{
-		public Person()
-		{
+		static int next_person_id = 0;
+		
+		int person_ID = next_person_id++;
+		string name;
+		Path current_path;
+		double distance_along_path = 0;
+		Route route;
+		
+		// Creates a person with a start and end node. This will give the person a
+		// default name.
+		public Person(Node start_node, Node end_node) : this("Steve", start_node, end_node) {
 		}
+		
+		// Creates a person with a start and end node and a name.
+		public Person(string name, Node start_node, Node end_node) {
+			this.name = name;
+			route = new Route(start_node, end_node);
+		}
+		
+		// Gets the path that the user is currently on.
+		public Path GetCurrentPath() {
+			return current_path;
+		}
+		
+		// Returns the distance along the path from the first node in the path array
+		// to the second (note that it's not in the direction of travel).
+		public double GetDistanceAlongPath() {
+			return distance_along_path;
+		}
+		
+		// Returns the name of the person.
+		public string GetName() {
+			return name;
+		}
+		
+		// Returns the ID of the person.
+		public int GetPersonID() {
+			return next_person_id;
+		}
+		
+		// Returns the route for the person.
+		public Route GetRoute() {
+			return route;
+		}
+		
 	}
 }
