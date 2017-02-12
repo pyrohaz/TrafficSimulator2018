3f5e02a874615acefa82037dec90dd871fd1df01
RichardInnocent <richardinnocent@sky.com>
2017-02-12 23:21:02 +0000

    Added comments and import statement

--- TrafficSimulator2018/People.cs
+++ TrafficSimulator2018/People.cs
@@ -7,6 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections.Generic;
 
 namespace TrafficSimulator2018
 {
@@ -20,6 +21,7 @@ namespace TrafficSimulator2018
 		
 		static People()
 		{
+			// Read in the people here
 		}
 		
 		public static List<People> GetPeople() {
