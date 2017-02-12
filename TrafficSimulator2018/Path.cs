9fb9251c1218ca417d02ef631e141e1b3b3baf89
RichardInnocent <richardinnocent@sky.com>
2017-02-12 23:58:26 +0000

    Added a variable to improve distance compute times

--- TrafficSimulator2018/Path.cs
+++ TrafficSimulator2018/Path.cs
@@ -19,14 +19,16 @@ namespace TrafficSimulator2018
 		static int next_route_ID = 0;
 		
 		int route_ID = next_route_ID++;
-		double speed_limit = 0;
 		Node [] nodes = new Node[2];
+		double speed_limit = 0;
+		double distance_between_nodes = 0;
 		
 		// Sets the start and end points and speed limit of the path.
 		public Path(Node node1, Node node2, double speed_limit) {
 			nodes[0] = node1;
 			nodes[1] = node2;
 			this.speed_limit = speed_limit;
+			distance_between_nodes = Math.Sqrt(Math.Pow(nodes[0].GetX() - nodes[1].GetX(), 2) + Math.Pow(nodes[0].GetY() - nodes[1].GetY(), 2));
 		}
 		
 		// Returns an array that contains the start and end points of the path.
@@ -40,8 +42,7 @@ namespace TrafficSimulator2018
 		}
 		
 		// Returns a double representing the speed limit of the path
-		public double GetSpeedLimit()
-		{
+		public double GetSpeedLimit() {
 			return speed_limit;
 		}
 		
@@ -55,10 +56,13 @@ namespace TrafficSimulator2018
 			return route_ID;
 		}
 		
+		// Returns the distance between the two end nodes of the path.
 		public double GetDistance(){
-			return Math.Sqrt(Math.Pow(nodes[0].GetX() - nodes[1].GetX(), 2) + Math.Pow(nodes[0].GetY() - nodes[1].GetY(), 2));
+			return distance_between_nodes;
 		}	
 		
+		// Returns the length of time (in s) that it should take to travel from one end of this
+		// node to the other.
 		public double GetTime(){
 			return GetDistance()/speed_limit;
 		}
