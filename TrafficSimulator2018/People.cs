339aff311a3825664a1d04d61af7b0b02e5a1391
RichardInnocent <richardinnocent@sky.com>
2017-02-12 23:17:59 +0000

    Created the people class

---
+++ TrafficSimulator2018/People.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: richa
+ * Date: 12/02/2017
+ * Time: 23:16
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TrafficSimulator2018
+{
+	/// <summary>
+	/// Description of People.
+	/// </summary>
+	public static class People
+	{
+		
+		static List<Person> people = new List<Person>();
+		
+		static People()
+		{
+		}
+		
+		public static List<People> GetPeople() {
+			return people;
+		}
+		
+		public static void addPerson(Person person) {
+			people.add(person);
+		}
+		
+	}
+}
