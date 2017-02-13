25158a68f2cdb0497f887a6e3f4f9cf145d4a924
Richard Innocent <richardinnocent@sky.com>
2017-02-13 11:38:32 +0000

    Added method descriptions

--- TrafficSimulator2018/Path.cs
+++ TrafficSimulator2018/Path.cs
@@ -24,7 +24,12 @@ namespace TrafficSimulator2018
 		double speed_limit = 0;
 		double distance_between_nodes = 0;
 		
-		// Sets the start and end points and speed limit of the path.
+		/// <summary>
+		/// Sets the start and end points and speed limit of the path upon initialisation.
+		/// </summary>
+		/// <param name="node1"></param>
+		/// <param name="node2"></param>
+		/// <param name="speed_limit"></param>
 		public Path(Node node1, Node node2, double speed_limit) {
 			nodes[0] = node1;
 			nodes[1] = node2;
@@ -32,17 +37,27 @@ namespace TrafficSimulator2018
 			distance_between_nodes = Math.Sqrt(Math.Pow(nodes[0].GetX() - nodes[1].GetX(), 2) + Math.Pow(nodes[0].GetY() - nodes[1].GetY(), 2));
 		}
 		
-		// Returns an array that contains the start and end points of the path.
+		/// <summary>
+		/// Returns an array of Nodes that contains the start and end points of the path.
+		/// The size of the returned Node array will always be 2.
+		/// </summary>
+		/// <returns></returns>
 		public Node [] GetNodes() {
 			return nodes;
 		}
 		
-		// Returns the ID of the path.
+		/// <summary>
+		/// Returns an int that represents the ID of the path.
+		/// </summary>
+		/// <returns></returns>
 		public int GetID() {
 			return path_ID;
 		}
 		
-		// Returns a double representing the speed limit of the path.
+		/// <summary>
+		/// Returns a double representing the speed limit of the path.
+		/// </summary>
+		/// <returns></returns>
 		public double GetSpeedLimit() {
 			return speed_limit;
 		}
@@ -52,23 +67,35 @@ namespace TrafficSimulator2018
 			this.speed_limit = speed_limit;
 		}
 		
-		// Returns the ID for the route.
+		/// <summary>
+		/// Returns an int representing the ID of the Path.
+		/// </summary>
+		/// <returns></returns>
 		public int GetPathID() {
 			return path_ID;
 		}
 		
-		// Returns the distance between the two end nodes of the path.
+		/// <summary>
+		/// Returns a double representing the distance between the two end Nodes of the Path.
+		/// </summary>
+		/// <returns></returns>
 		public double GetDistance() {
 			return distance_between_nodes;
 		}	
 		
-		// Returns the length of time (in s) that it should take to travel from one end of this.
-		// node to the other.
+		/// <summary>
+		/// Returns a double representing the length of time (in s) that it should take to travel
+		/// from one end of this Path to the other.
+		/// </summary>
+		/// <returns></returns>
 		public double GetTime() {
 			return GetDistance()/speed_limit;
 		}
 		
-		// Returns some text that describes the path.
+		/// <summary>
+		/// Returns a string that describes the Path.
+		/// </summary>
+		/// <returns></returns>
 		public override string ToString() {
 			return "Path " + path_ID + ":\nNode 1: " + nodes[0].GetID() + "\nNode 2: " + nodes[1].GetID() + "\nSpeed limit: " + GetSpeedLimit() +
 				"\nDistance between nodes: " + distance_between_nodes + "\nTime between nodes (s): " + GetTime() + "\n\n";
