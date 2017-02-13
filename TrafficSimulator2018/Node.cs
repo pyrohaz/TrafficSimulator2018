7343873efd2fbd4b125affe3358a5a6a0563cd6f
Richard Innocent <richardinnocent@sky.com>
2017-02-13 11:34:16 +0000

    Changed SetVisitors function to IncrementVisitors to ensure data integrity. Method descriptions were added.

--- TrafficSimulator2018/Node.cs
+++ TrafficSimulator2018/Node.cs
@@ -11,63 +11,104 @@ using System;
 namespace TrafficSimulator2018
 {
 	/// <summary>
-	/// Description of Node.
+	/// A Node is a junction between different Paths. It contains an x and y position
+	/// on the map.
 	/// </summary>
 	public class Node
 	{
 		
-		double x, y;
-		int visitors, id;
+		double x = 0.0, y = 0.0;
+		int visitors = 0, id = -1;
 		
-		public Node()
-		{
-			id = -1;
-			visitors = 0;
-			x = 0.0;
-			y = 0.0;
-		}
+		/// <summary>
+		/// Default constructor sets a node with default values.
+		/// </summary>
+		public Node() {}
 		
-		public Node(int ID, double X, double Y){
+		/// <summary>
+		/// Constructor that allows the ID and coordinates to be set on initialisation.
+		/// </summary>
+		/// <param name="ID"></param>
+		/// <param name="X"></param>
+		/// <param name="Y"></param>
+		public Node(int ID, double X, double Y) {
 			id = ID;
 			x = X;
 			y = Y;
 		}
 		
-		//Sets
-		public void SetID(int ID){
+		/// <summary>
+		/// This method should be called if a Person passes through the Node. This will increment
+		/// the visitor counter, that counts how many Persons have passed through the node.
+		/// </summary>
+		/// <returns></returns>
+		public int IncrementVisitors() {
+			return ++visitors;
+		}
+		
+		/// <summary>
+		/// Sets the ID of the node.
+		/// </summary>
+		/// <param name="ID"></param>
+		public void SetID(int ID) {
 			id = ID;
 		}	
 		
-		public void SetVisitor(int V){
-			visitors = V;
-		}
-		
-		public void SetX(double X){
+		/// <summary>
+		/// Sets the x coordinate of the node on the Map. If the x coordinate was never set, this
+		/// will be 0.0.
+		/// </summary>
+		/// <param name="X"></param>
+		public void SetX(double X) {
 			x = X;
 		}
 		
-		public void SetY(double Y){
+		/// <summary>
+		/// Sets the y coordinate of the node on the Map. If the x coordinate was never set, this
+		/// will be 0.0.
+		/// </summary>
+		/// <param name="Y"></param>
+		public void SetY(double Y) {
 			y = Y;
 		}
 		
-		//Gets
-		public int GetID(){
+		/// <summary>
+		/// Returns an int representing the ID of the Node. If the ID has never been set, this will
+		/// return -1.
+		/// </summary>
+		/// <returns></returns>
+		public int GetID() {
 			return id;
 		}
 		
-		public int GetVisitors(){
+		/// <summary>
+		/// Returns an int representing the number of Persons that have passed through this node.
+		/// </summary>
+		/// <returns></returns>
+		public int GetVisitors() {
 			return visitors;
 		}
 		
-		public double GetX(){
+		/// <summary>
+		/// Returns a double representing the x coordinate of the node on the Map.
+		/// </summary>
+		/// <returns></returns>
+		public double GetX() {
 			return x;
 		}
 		
+		/// <summary>
+		/// Returns a double representing the y coordinate of the node on the Map.
+		/// </summary>
+		/// <returns></returns>
 		public double GetY(){
 			return y;
 		}
 		
-		// Returns some text that describes the node.
+		/// <summary>
+		/// Returns some text that describes the node.
+		/// </summary>
+		/// <returns></returns>
 		public override string ToString() {
 			return "Node " + id + ":\nx: " + x + "\ny: " + y + "Visitor count: " + visitors + "\n\n";
 		}
