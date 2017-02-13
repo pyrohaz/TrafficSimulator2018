7fe6ae0e030f351d88297d5599a03c78495e9a90
RichardInnocent <richardinnocent@sky.com>
2017-02-13 00:23:36 +0000

    Began a route calculation implementation

--- TrafficSimulator2018/Route.cs
+++ TrafficSimulator2018/Route.cs
@@ -7,6 +7,7 @@
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
 using System;
+using System.Collections.Generic;
 
 namespace TrafficSimulator2018
 {
@@ -16,5 +17,28 @@ namespace TrafficSimulator2018
 	public class Route
 	{
 		
+		public Route(Node start_node, Node end_node) {
+			
+			List<Node> nodes = Map.GetNodes();
+			List<NodeAndTime> nodesAndTimes = new List<NodeAndTime>(nodes.Count);
+			
+			for (int i = 0; i < nodes.Count; i++) {
+				nodesAndTimes.Add(new NodeAndTime(nodes[i]));
+			}
+			
+		}
+		
 	}
+	
+	struct NodeAndTime {
+		Node node;
+		double time;
+		
+		public NodeAndTime(Node node) {
+			this.node = node;
+			time = Double.MaxValue;
+		}
+		
+	}
+	
 }
