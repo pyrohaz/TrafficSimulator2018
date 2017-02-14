caa736bbc9fbae02e6742ccebddfbac81dd0542f
Richard Innocent <richardinnocent@sky.com>
2017-02-14 10:04:50 +0000

    Added a PseudoNode class to define a Person's position

---
+++ TrafficSimulator2018/PseudoNode.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Richard
+ * Date: 14/02/2017
+ * Time: 10:03
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace TrafficSimulator2018
+{
+	/// <summary>
+	/// Description of PseudoNode.
+	/// </summary>
+	public class PseudoNode : Node {
+		
+		public PseudoNode() {
+		}
+	}
+}
